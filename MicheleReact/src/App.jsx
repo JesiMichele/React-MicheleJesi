b824ed73c19b3e024cce8f955f6f469a3b1df956
JesiMichele <144861324+JesiMichele@users.noreply.github.com>
2024-01-23 21:54:47 -0300

    Update App.jsx

--- MicheleReact/src/App.jsx
+++ MicheleReact/src/App.jsx
@@ -1,18 +1,25 @@
 
 import './App.css'
 import ItemListcontainer from './components/ItemListcontainer/ItemListcontainer'
-
-
-
-
+import NavBar from './components/NavBar/NavBar'
 
 function App() {
-  
 
   return (
+
+    <>
+
+    <NavBar/>
+
     <ItemListcontainer greeting={"Bienvenidos a mi e-commerce"}/>
-     
+
+    </>
+
   )
+
 }
 
 export default App
+
+
+
